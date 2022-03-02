30fdd7d89615e7be1e8d25e1f2651dd5e2ded727
Callum Stott <callum@seadowg.com>
2022-03-02 17:50:57 +0000

    No need to copy env in and out now it's gitignored

--- upgrade.sh
+++ upgrade.sh
@@ -3,15 +3,10 @@
 set -e
 
 pushd ~/central
-  cp .env ~/
   docker-compose stop
 
-  git reset HEAD --hard
-  git checkout master
   git pull
   git submodule update -i
-
-  cp ~/.env .
 
   docker-compose build
   docker-compose up -d
